d1323b0e6b3536c8493d5eff58fb2b319e7e4065
tsammereyer <tobias418@gmail.com>
2018-04-14 23:09:06 +0200

    First Commit - tried some stuff

---
+++ README.md
@@ -0,0 +1,13 @@
+# Binary Heap and DHeap
+implementation of a Binary Heap and DHeap in Java  
+for building check out the build.xml -> ant must be installed on your pc
+
+## Commands to run the Code
+"ant run" compiles and executes the Java Application  
+"ant jar" creates Java Package  
+"ant runjar" runs the JAR  
+"ant clean" deletes all build artefacts  
+
+## Algorithm duration
+The duration is calculated with System.nanoTime();  
+values are in milliseconds
---
+++ src/swe4/Codemetrics.java
@@ -0,0 +1,50 @@
+package swe4;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+import com.sun.org.apache.bcel.internal.classfile.ClassParser;
+import com.sun.org.apache.bcel.internal.classfile.Field;
+import com.sun.org.apache.bcel.internal.classfile.JavaClass;
+import com.sun.org.apache.bcel.internal.classfile.Method;
+
+public class Codemetrics {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		importJarFile();
+
+	}
+
+	public static void importJarFile() throws IOException {
+		String jarFileName = "heap.jar";
+		JarFile jarfile = new JarFile(jarFileName);
+		JarFile jf = new JarFile(new File(jarFileName));
+
+		Enumeration<JarEntry> entries = jf.entries();
+		JarEntry element;
+		while (entries.hasMoreElements()) {
+			element = entries.nextElement();
+			if (element.getName().endsWith(".class")) { // !element.isDirectory()
+				System.out.println(element.getName());
+
+				ClassParser parser = new ClassParser(jarFileName, element.getName());
+				JavaClass javaClass = parser.parse();
+
+				System.out.println("Class: " + javaClass.getClassName());
+				
+				System.out.println("  Methods:");
+				for (Method method : javaClass.getMethods()) {
+					System.out.println("    " + method);
+				}
+				
+				javaClass.ge
+			}
+
+		}
+	}
+
+}
